d6af999a64ed93e7a01b626952c1395f509f8612
elmehdibouhou <64345019+elmehdibouhou@users.noreply.github.com>
2020-05-15 00:05:54 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {BrowserRouter as Router} from 'react-router-dom';  
+import {ProductProvider} from "./context"; 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProductProvider>
+    <Router>
+        <App />
+    </Router>
+  </ProductProvider>,
+  
   document.getElementById('root')
 );
 
